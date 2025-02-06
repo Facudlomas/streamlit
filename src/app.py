0b20c35ef40b90d93403f7cc5d4936111b1a0f1d
Facudlomas <Facundo.mosquera.cala@gmail.com>
2025-02-06 16:18:38 +0000

    a medias

--- src/app.py
+++ src/app.py
@@ -1,4 +1,16 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
 
-# your code here
+@st.cache(ttl=60*5, max_entries=20)
+def load_data():
+    data = pd.read_csv('/workspaces/streamlit/data/decision_tree_classifier_default_42.sav')
+    return data
+
+data = load_data()
+
+st.markdown('<style>description{color:green;}</style>', unsafe_allow_html=True)
+st.title('prediccion de diabetes')
+st.markdown("<description>con algunos parametros podemos predecir en si tiene posibilidad de tener diabetes </description>", unsafe_allow_html=True)
+st.sidebar.title('Select')
